8526d2953e007f13d44d782a3733d3cb7889a206
donnymarsahid <donnymrshd.94@gmail.com>
2021-10-06 19:59:58 +0700

    updates server

--- src/sendEmail/sendEmail.js
+++ src/sendEmail/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const { transaction, user } = require("../../models");
+const convertRupiah = require("rupiah-format");
 
 async function sendResetLink(email, subject, link, fullname) {
   try {
@@ -41,8 +42,6 @@ async function sendResetLink(email, subject, link, fullname) {
 
 async function sendStatusPayment(orderId, status) {
   try {
-    console.log("hello world " + orderId);
-
     const data = await transaction.findOne({
       where: {
         id: orderId,
@@ -69,14 +68,18 @@ async function sendStatusPayment(orderId, status) {
       from: process.env.EMAIL,
       to: data.user.email,
       subject: "Payment Status",
-      html: `<div>
-        <ul>
-            <li>${data.user.fullname}</li>
-            <li>${data.user.email}</li>
-            <li>${orderId}</li>
-            <li>${status}</li>
-        </ul>
-      </div>`,
+      html: `<div style="background-color: #f6f6f6; padding: 35px; font-family: sans-serif; color: #3d3d3d">
+      <div style="background-color: white; height: 400px; padding: 10px 25px">
+        <div style="border-radius: 5px; background-color: rgb(189, 7, 7); color: white; padding: 3px; justify-content: center; display: flex; align-items: center">
+          <h3 style="letter-spacing: 1px">Waysbucks Coffee</h3>
+        </div>
+        <h4>Hi, ${data.user.fullname}</h4>
+        <p>Order ID : ${orderId}</p>
+        <p>Status Payment : ${status}</p>
+        <p>Total : ${convertRupiah.convert(data.total)}</p>
+        <p>Check Now : <a href="https://8e10-180-243-2-155.ngrok.io/all-transaction" style="color: rgb(189, 7, 7)">click here!</a></p>
+      </div>
+    </div>`,
     };
 
     await transporter.sendMail(options, function (err, data) {
