11894f58ae206c0644cd592195447dc28e9e3e6e
Andy mgindotech <andy.mgindotech@gmail.com>
2022-06-24 15:35:25 +0700

    first commit deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000
 const cors = require('cors');
 
 const http = require('http');
