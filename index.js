145f540390584a3f1c0c893070946307d47adaaf
DON <donnymrshd.94@gmail..com>
2022-07-24 14:58:45 +0700

    fixing midtrans

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://dn-waysbucks.netlify.app',
   },
 });
 
